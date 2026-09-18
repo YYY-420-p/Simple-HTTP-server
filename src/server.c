e3fca29998c3273720548a2ff6b67b448a168b24
7xD00rs <yyy153920@gmail.com>
2026-09-19 00:02:09 +0100

    Implement socket creation in server.c
    Added socket creation and error handling for server.

--- src/server.c
+++ src/server.c
@@ -19,4 +19,26 @@ int socket_fd = {0};
 int client_fd = {0};
 char buffer[256] = {0} ;
 
+ // creating a socket and checking for the return value
+     socket_fd = socket(AF_INET , SOCK_STREAM , 0 );
+    if ( socket_fd < 0){
+      printf(RED "SOCKET ERROR: %d\n" , errno);
+      perror("ERROR");
+      exit(EXIT_FAILURE);
+    }
+
+    // filling socket parameters
+    struct sockaddr_in server_addr ;
+    server_addr.sin_family = AF_INET ;
+    server_addr.sin_port   = htons(8080) ;
+    server_addr.sin_addr.s_addr = INADDR_ANY ;
+
+ 
+
+
+
+
+
+
+
 }
