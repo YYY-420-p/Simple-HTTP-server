562abdf7eb54ef34c219b31693a79ca7db2fac03
7xD00rs <yyy153920@gmail.com>
2026-09-21 01:03:42 +0100

    Implement socket binding for client
    Added socket binding for client with error handling.

--- src/client.c
+++ src/client.c
@@ -19,3 +19,20 @@ int main() {
                 printf("socket error : %d\n" , errno);
                 exit(EXIT_FAILURE);
         }
+// infos about this socket
+        struct sockaddr_in client_addr ;
+        client_addr.sin_family = AF_INET ;
+        client_addr.sin_port = htons (8080) ;
+        client_addr.sin_addr.s_addr = INADDR_ANY ;
+
+// linking socket and checking for errors
+         if (bind(client_fd , (struct sockaddr*)&client_addr , sizeof(client_addr)) < 0 ) {
+                 
+                 printf( "Error on binding : %d\n" , errno);
+                 exit(EXIT_FAILURE);
+                 
+         }
+
+                 }
+
+
